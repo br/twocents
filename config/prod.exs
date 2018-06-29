6ecf9d9a622e1f91db214846e58e1855f79367fb
Mariko Sampaga <msampaga@msampaga-br-sf-mbp.local>
2018-06-29 13:02:05 -0700

    prod.exs edit

--- config/prod.exs
+++ config/prod.exs
@@ -65,6 +65,6 @@ config :twocents, Twocents.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: System.get_env("POSTGRES_USERNAME") || "msampaga",
   password: System.get_env("POSTGRES_PASSWORD") || "postgres",
-  database: "moderation_prod",
+  database: "twocents_dev",
   hostname: System.get_env("POSTGRES_HOSTNAME") || "db",
   pool_size: 10
