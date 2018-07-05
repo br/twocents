9bf7ebcc85c51f7226acb2362c818c2451a3166f
Mariko Sampaga <msampaga@msampaga-br-sf-mbp.local>
2018-07-05 14:56:44 -0700

    test hex.outdated

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule Twocents.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:credo, "~> 0.8.10", runtime: false}
+      {:credo, "~> 0.8.10"}
     ]
   end
 
