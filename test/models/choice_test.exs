e1fbf694ac76245abfbaedfb6083368f172b2b52
Mariko Sampaga <msampaga@msampaga-br-sf-mbp.local>
2018-07-11 10:05:22 -0700

    including choice test

---
+++ test/models/choice_test.exs
@@ -0,0 +1,25 @@
+defmodule Twocents.ChoiceTest
+  use Twocents.ModelCase
+
+  alias Twocents.Choice
+
+  @valid_attrs %{votes: 42, title: "some title"}
+  @invalid_attrs %{}
+
+  setup do
+    poll = %Poll{title: "Hello World" |> Repo.insert!
+    %Choice{title: "Hello to you too!", poll_id: poll.id }
+    |> Repo.insert!
+    %Choice{title: "How are you doing?", poll_id: poll.id }
+    |> Repo.insert!
+
+    {:ok, poll: poll}
+  end
+
+  test "retreive choice counts", context do
+    poll = Poll
+    |> Repo.get(context[:poll].id)
+    |> Repo.preload(:choices)
+
+    assert Enum.count(poll.choices) == 2
+  end
