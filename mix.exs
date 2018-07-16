972feb57912aca726bf818960228093cfe52ac62
Mariko Sampaga <msampaga@msampaga-br-sf-mbp.local>
2018-07-16 15:47:57 -0700

    including dep HTTPoison

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule Twocents.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:httpoison, "~> 0.13"},
       {:phoenix, "~> 1.3.2"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
