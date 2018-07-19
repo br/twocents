9f0af65c32034c1579dd40b8690c8f83d4427abf
Mariko Sampaga <msampaga@msampaga-br-sf-mbp.local>
2018-07-19 14:16:52 -0700

    unique_constraint

--- lib/twocents_web/models/poll.ex
+++ lib/twocents_web/models/poll.ex
@@ -21,7 +21,7 @@ defmodule Twocents.Poll do
     model
     |> cast(params, [:title, :closed])
     |> validate_required([:title, :closed])
-    |> unsafe_validate_unique(:title)
+    |> unique_constraint(:title)
     |> cast_assoc(:choices)
   end
 end
