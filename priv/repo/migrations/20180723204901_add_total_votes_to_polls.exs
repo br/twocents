cab71bedd40296166bb783967a50bfdf317c5c0c
Mariko Sampaga <mariko.sampaga@bleacherreport.com>
2018-07-25 13:00:06 -0700

    removing add_total_votes_to_polls.exs

--- priv/repo/migrations/20180723204901_add_total_votes_to_polls.exs
+++
@@ -1,9 +0,0 @@
-defmodule Twocents.Repo.Migrations.AddTotalVotesToPolls do
-  use Ecto.Migration
-
-  def change do
-    alter table(:polls) do
-      add :totalvotes, :integer, default: 0
-    end
-  end
-end
