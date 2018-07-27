285c6c6fcbaba0cda6c3c96eefa40ccd693ff36f
Mariko Sampaga <mariko.sampaga@bleacherreport.com>
2018-07-27 14:27:01 -0700

    consolidate GET requests to RESOURCES

--- lib/twocents_web/router.ex
+++ lib/twocents_web/router.ex
@@ -24,8 +24,6 @@ defmodule TwocentsWeb.Router do
    scope "/test", TwocentsWeb do
      pipe_through :api
      resources "/polls", PollController
-     # get "/polls", PollController, :index
-     # get "/polls/:id", PollController, :show
      post "/polls", PollController, :create #to save new polls to db
      put "/polls", PollController, :update #to increment poll votes
    end
