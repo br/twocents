230f68472830641bbc7deb8293603c3346a54bd7
Mariko Sampaga <mariko.sampaga@bleacherreport.com>
2018-08-08 10:30:58 -0700

    add choices button/limit choices to 4 max, 2 min

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,55 +17,6 @@ import "react-phoenix"
 import React from "react"
 import ReactDOM from "react-dom"
 
-class Question extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event){
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event){
-    var title = this.state.value;
-    alert('A poll title was submitted: ' + title);
-    event.preventDefault();
-  }
-
-  render(){
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Question:
-          <input type="text" placeholder="Poll Title" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-        </form>
-    );
-  }
-}
-
-//=====================
-ReactDOM.render(
-  <Question />,
-  document.getElementById('root')
-);
-
-// class HelloReact extends React.Component{
-//   render(){
-//     return(<h1>Hello React!</h1>);
-//   }
-// }
-//
-// ReactDOM.render(
-//   <HelloReact/>, document.getElementById("hello-react")
-// )
-
-
 // Import local files
 //
 // Local files can be imported directly using relative
