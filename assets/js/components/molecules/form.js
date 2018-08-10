6b8bc5205baf49a0af9e9f0093591eff5987b5d1
Mariko Sampaga <mariko.sampaga@bleacherreport.com>
2018-08-10 15:28:34 -0700

    improving on FE, creating handleQuestionChange and in process of implementing

--- assets/js/components/molecules/form.js
+++ assets/js/components/molecules/form.js
@@ -1,44 +1,52 @@
 import React from "react";
 import Choice from '../atoms/choice';
 import { createPoll } from "../../api/twocents_api";
-
+/*
+Form for user to add new poll to the database
+*/
 class Form extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       question: '',
-      choices:["",""]
+      choices:["",""] //Array for choices, default 2 choices
     };
+    //While user is typing in choices
     this.handleChoiceChange = this.handleChoiceChange.bind(this);
+    //When user presses Submit button
     this.handleSubmit = this.handleSubmit.bind(this);
+    //When user clicks Add Choices button
     this.addChoices = this.addChoices.bind(this);
   }
-
+  //While user is typing in choices
   handleChoiceChange(event) {
+    //copies text in input box
     const value = event.target.value;
+    //each input box assigned an index for differentiation
     const idx = event.target.name;
     console.log("handleChoiceChange");
+    //edits choices array via index to text in input box
     this.setState((prevState) => {
       const choices = [...prevState.choices];
+      //sets text in input box to choice array via same index
       choices[idx] = value;
       return {
         choices: choices
       };
     });
   }
-
+  //When user presses Submit button
   handleSubmit(event){
+    //does not allow default to be submitted as a new poll
     event.preventDefault();
     console.log("calling createPoll");
+    //formats question and choices appropriately for html POST request
     createPoll(this.state.question, this.state.choices);
     debugger;
-    // var title = this.state.question;
-    // var choice = this.state.choice;
-    // alert('A poll was submitted: ' + title + ' with choice ' + choice);
   }
-
+  //When user clicks Add Choices button
   addChoices(){
-    if(this.state.choices.length < 4){ // Will need to use Object.keys(this.state.choices).length
+    if(Object.keys(this.state.choices).length < 4){ // **** Will need to use Object.keys(this.state.choices).length
       this.setState({
         choices: [ ...this.state.choices, "" ]
       });
