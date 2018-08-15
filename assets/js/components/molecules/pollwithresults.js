f146a6b9cad5cb4e79bc78f1ea42a652edc0f8d7
Mariko Sampaga <mariko.sampaga@bleacherreport.com>
2018-08-15 15:48:54 -0700

    adding buttons

--- assets/js/components/molecules/pollwithresults.js
+++ assets/js/components/molecules/pollwithresults.js
@@ -3,11 +3,11 @@ import React from "react";
 Displays whole database and individual polls
 */
 class PollWithResults extends React.Component {
-
   render(){
     return (
       <div>
-        <h3> { this.props.poll.title } </h3>
+        <h4> { this.props.poll.poll_id } </h4>
+        <input type="button" value={ this.props.poll.title } id="show_button" />
       </div>
     );
   }
